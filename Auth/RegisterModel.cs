adad010562259bbdd6b195c5bdd8c50abc1d5a34
TessaHayden <tessahayden3@gmail.com>
2023-11-25 19:35:18 -0800

    Auth directory added for JWT Authentication

---
+++ Auth/RegisterModel.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Build.Framework;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace AnimalShelterApi.Auth
+{
+  public class RegisterModel
+  {
+    [Required(ErrorMessage = "User Name is required")]
+    public string Username { get; set; }
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is required")]
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Password is Required")]
+    public string Password { get; set; }
+  }
+}
---
+++ Auth/Response.cs
@@ -0,0 +1,8 @@
+namespace AnimalShelterApi.Auth
+{
+  public class Response
+  {
+    public string Status { get; set; }
+    public string Message { get; set; }
+  }
+}
